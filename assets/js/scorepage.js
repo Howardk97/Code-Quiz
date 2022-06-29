fcfb41258836c25ed1679ea89bbec850b94891e3
KimboSlice <kimberlyhoward529@gmail.com>
2022-06-29 16:49:02 -0400

    I added comments to each of the files

--- assets/js/scorepage.js
+++ assets/js/scorepage.js
@@ -1,5 +1,8 @@
+// Set variable to hold the container for user score
 var userScoreContainer = document.getElementById("user-score");
 
+// set a variable to the score in local storage
 var finalScore = localStorage.getItem("currentScore")
 
+// Display the score
 userScoreContainer.textContent = "Final Score: " + finalScore;
--- assets/js/script.js
+++ assets/js/script.js
@@ -104,29 +104,31 @@ function quesOne(event) {
     } 
 }
 
-
+// function that sets page and functioning for question 2
 function quesTwo(event) {
+    // Set header on page
     startTitle.textContent = "How do you define an empty array named myArray?";
     startDescription.textContent = "";
-    // console.log(response);
 
+    // Set text for answer options
     q2Option1.textContent = "A) var myArray = [];";
     q2Option2.textContent = "B) myArray == []";
     q2Option3.textContent = "C) Array myArray = ();";
     q2Option4.textContent = "D) Array myArray = ()";
     
+    // append text to buttons
     startDescription.appendChild(q2Option1);
     startDescription.appendChild(q2Option2);
     startDescription.appendChild(q2Option3);
     startDescription.appendChild(q2Option4);
 
+    // Style text for answer buttons
     q2Option1.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
     q2Option2.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
     q2Option3.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
     q2Option4.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
     
-    // console.log(q1Option1.textContent);
-    // console.log(event.target.textContent === q1Option1.textContent);
+    // Have page go to question 3 content if any answer option is selected
     if (event.target.textContent === q2Option1.textContent) {
         quesThree(event);
     } else if (event.target.textContent === q2Option2.textContent) {
@@ -137,58 +139,85 @@ function quesTwo(event) {
         quesThree(event);
     }
 
+    // If user selects first option on question 1,
     if (event.target.textContent === q1Option1.textContent) {
+        // Display "wrong" on question 2 page
         checkAnswer.textContent = "Wrong!"
 
+        // Style "wrong"
         checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
+        // Deduct 10 seconds from time
         timeLeft = timeLeft - 10;
 
-    } else if (event.target.textContent === q1Option2.textContent) {
+    } 
+    
+    // If user selects second option on question 1,
+    else if (event.target.textContent === q1Option2.textContent) {
+        // Display "wrong" on question 2 page
         checkAnswer.textContent = "Wrong!";
 
+        // Style "wrong"
         checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
+        // Deduct 10 seconds from time
         timeLeft = timeLeft - 10;
 
-    } else if (event.target.textContent === q1Option3.textContent) {
+    } 
+    
+    // If user selects third option on question 1,
+    else if (event.target.textContent === q1Option3.textContent) {
+        // display "correct" on question 2 page
         checkAnswer.textContent = "Correct!";
 
+        // Style "correct"
         checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
+        // Add 25 points to score
         currentScore = currentScore + 25;
 
+        // Set points to currentScore in localStorage
         localStorage.setItem("currentScore", currentScore);
 
-        console.log(currentScore);
-
-    } else if (event.target.textContent === q1Option4.textContent) {
+    } 
+    // If user selects fourth option on question 1,
+    else if (event.target.textContent === q1Option4.textContent) {
+        // display "wrong" on screen
         checkAnswer.textContent = "Wrong!";
 
+        // style "wrong"
         checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
+        // Deduct 10 seconds from time
         timeLeft = timeLeft - 10;
     }
 }
 
+// function that generates third question
 function quesThree(event) {
+    // Third question
     startTitle.textContent = "How do you define an empty function named myFunction?";
     startDescription.textContent = "";
+    
+    // Text for answer options
     q3Option1.textContent = "A) myFunction";
     q3Option2.textContent = "B) function myFunction {}";
     q3Option3.textContent = "C) function myFunction () {}";
     q3Option4.textContent = "D) function = myFunction";
 
+    // Append text to buttons
     startDescription.appendChild(q3Option1);
     startDescription.appendChild(q3Option2);
     startDescription.appendChild(q3Option3);
     startDescription.appendChild(q3Option4);
 
+    // Style buttons
     q3Option1.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
     q3Option2.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
     q3Option3.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
     q3Option4.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
 
+    // Have page go to question 4 if any answer option is clicked
     if (event.target.textContent === q3Option1.textContent) {
         quesFour(event);
     } else if (event.target.textContent === q3Option2.textContent) {
@@ -199,58 +228,86 @@ function quesThree(event) {
         quesFour(event);
     }
 
+    // If user clicks first option on question 2,
     if (event.target.textContent === q2Option1.textContent) {
+        // Display "correct" on question 3 page
         checkAnswer.textContent = "Correct!"
 
+        // Style response
         checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
+        // Add points to score
         currentScore = currentScore + 25;
 
-        console.log(currentScore);
-
+        // Store current user score in local storage
         localStorage.setItem("currentScore", currentScore);
 
-    } else if (event.target.textContent === q2Option2.textContent) {
+    } 
+    
+    // If user clicks second option on question 2,
+    else if (event.target.textContent === q2Option2.textContent) {
+        // Display "wrong" on question 3 page
         checkAnswer.textContent = "Wrong!";
 
+        // Style response
         checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
+        // deduct 10 seconds from time
         timeLeft = timeLeft - 10;
 
-    } else if (event.target.textContent === q2Option3.textContent) {
+    } 
+    
+    // If user clicks third option on question 2,
+    else if (event.target.textContent === q2Option3.textContent) {
+        // Display "wrong" on question 3 page
         checkAnswer.textContent = "Wrong!";
 
+        // Style response
         checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
+        // deduct 10 seconds from time
         timeLeft = timeLeft - 10;
 
-    } else if (event.target.textContent === q2Option4.textContent) {
+    } 
+    
+    // If user clicks fourth option on question 2,
+    else if (event.target.textContent === q2Option4.textContent) {
+        // Display "wrong" on question 3 page
         checkAnswer.textContent = "Wrong!";
 
+        // Style response
         checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
+        // deduct 10 seconds from time
         timeLeft = timeLeft - 10;
     }
 }
 
+// function that generates page for question 4
 function quesFour (event) {
+    // Question 4
     startTitle.textContent = "Which coding language plays the role of the brain of the webpage?";
     startDescription.textContent = "";
+
+    // Text for nswer options
     q4Option1.textContent = "A) html";
     q4Option2.textContent = "B) javaScript";
     q4Option3.textContent = "C) css";
     q4Option4.textContent = "D) Webpages don't have brains.";
 
+    // Append text to buttons
     startDescription.appendChild(q4Option1);
     startDescription.appendChild(q4Option2);
     startDescription.appendChild(q4Option3);
     startDescription.appendChild(q4Option4);
 
+    // Style text
     q4Option1.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
     q4Option2.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
     q4Option3.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
     q4Option4.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
 
+    // Send user to score page if any answer option is selected
     if (event.target.textContent === q4Option1.textContent) {
         scorePage(event);
     } else if (event.target.textContent === q4Option2.textContent) {
@@ -261,40 +318,61 @@ function quesFour (event) {
         scorePage(event);
     }
 
+    // If option 1 on question 3 was selected
     if (event.target.textContent === q3Option1.textContent) {
+        // display response
         checkAnswer.textContent = "Wrong!"
 
+        // style response
         checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
+        // deduct 10 seconds from time
         timeLeft = timeLeft - 10;
 
-    } else if (event.target.textContent === q3Option2.textContent) {
+    } 
+    
+    // If option 2 on question 3 was selected
+    else if (event.target.textContent === q3Option2.textContent) {
+        // display response
         checkAnswer.textContent = "Wrong!";
 
+        // style response
         checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
+        // Deduct 10 seconds from time
         timeLeft = timeLeft - 10;
 
-    } else if (event.target.textContent === q3Option3.textContent) {
+    } 
+    
+    // If option 3 on question 3 was selected
+    else if (event.target.textContent === q3Option3.textContent) {
+        // display repsonse
         checkAnswer.textContent = "Correct!";
 
+        // style response
         checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
+        // add 25 points to score
         currentScore = currentScore + 25;
 
+        // store score in local storage
         localStorage.setItem("currentScore", currentScore);
-
-        console.log(currentScore);
-
-    } else if (event.target.textContent === q3Option4.textContent) {
+    } 
+    
+    // If option 4 on question 3 was selected
+    else if (event.target.textContent === q3Option4.textContent) {
+        // diplay response
         checkAnswer.textContent = "Wrong!";
 
+        // style response
         checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
+        // deduct 10 seconds from time
         timeLeft = timeLeft - 10;
     }    
 }
 
+// Create elements for score page
 var score = document.createElement("p");
 var initialsContainer = document.createElement("div");
 var initialsLabel = document.createElement("span");
@@ -302,56 +380,62 @@ var initialsPromp = document.createElement("textarea");
 var submitSection = document.createElement("div");
 var submitButton = document.createElement("button");
 
+// assign a variable to the timer in the header
 var quizTimer = document.getElementById("time");
 
+// run function that generates score page
 function scorePage(event) {
+    // text at top
     startTitle.textContent = "All done!";
     startDescription.textContent = "";
     checkAnswer.textContent = "";
+    
+    // Stop timer
     quizTimer.textContent = "Timer: 0 seconds left.";
 
+    // display score
     startDescription.appendChild(score);
+
+    // attach created elements on html
     startDescription.appendChild(initialsContainer);
     initialsContainer.appendChild(initialsLabel);
     initialsContainer.appendChild(initialsPromp);
     startDescription.appendChild(submitSection);
     submitSection.appendChild(submitButton);
 
+    // Submit button
     submitButton.textContent = "Submit";
     submitButton.setAttribute("style", "background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 20%;");
-    // console.log(startDescription);
 
 
-    // submitButton.setAttribute("style", "display: flex; align-items: center; width: 5%;");
-
+    // If option 2 was selected on question 4,
     if (event.target.textContent === q4Option2.textContent) {
+        // add 25 points to the score
         currentScore = currentScore + 25;
 
+        // Store points in local storage
         localStorage.setItem("currentScore", currentScore);
+    } 
 
-        console.log(currentScore);
-
-    } else {
+    // Store current score as the score
+    else {
         localStorage.setItem("currentScore", currentScore);
-
-        console.log(currentScore);
     }
 
+    // Display score
     score.textContent = "Score: " + currentScore;
+
+    // text for initials
     initialsLabel.textContent = "Initials: "
 }
 
-var finalScore = localStorage.getItem("quesFourScore");
-// var endScore = (finalScore).toString();
-// console.log(finalScore);
+// set final score as the variable to hold the score
+// var finalScore = localStorage.getItem("quesFourScore");
 
-// var userScore = finalscores.getElementById("user-score");
-// console.log(userScore);
-
+// function that is generated when submit button is clicked
 function submitBtn() {
+    // attach java to other html
     window.location.href="finalscores.html";
-
-    userScore.textContent = endScore;
 }
 
 startButton.addEventListener("click", strtBtn)
