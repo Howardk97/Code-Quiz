2df9450639c4b71e419a753113064812cab3bb05
KimboSlice <kimberlyhoward529@gmail.com>
2022-06-28 20:10:41 -0400

    Restructured javaScript code so a response tells user if their previous answer was correct.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,103 +3,280 @@ var startButton = document.getElementById("start-button");
 console.log(startButton);
 var startTitle = document.getElementById("start-title");
 var startDescription = document.getElementById("start-description");
+var response = document.getElementById("disp");
 console.log(startTitle.textContent);
 
 // console.log(startBoxHeader);
 //  var quesOne = "What is a comment?";
-var option1 = document.createElement("button");
-var option2 = document.createElement("button");
-var option3 = document.createElement("button");
-var option4 = document.createElement("button");
+var q1Option1 = document.createElement("button");
+var q1Option2 = document.createElement("button");
+var q1Option3 = document.createElement("button");
+var q1Option4 = document.createElement("button");
+var q2Option1 = document.createElement("button");
+var q2Option2 = document.createElement("button");
+var q2Option3 = document.createElement("button");
+var q2Option4 = document.createElement("button");
+var q3Option1 = document.createElement("button");
+var q3Option2 = document.createElement("button");
+var q3Option3 = document.createElement("button");
+var q3Option4 = document.createElement("button");
+var q4Option1 = document.createElement("button");
+var q4Option2 = document.createElement("button");
+var q4Option3 = document.createElement("button");
+var q4Option4 = document.createElement("button");
 
-function btnClick() {
+localStorage.setItem("Ques1Opt1", "Wrong!");
+localStorage.setItem("Ques1Opt2", "Wrong!");
+localStorage.setItem("Ques1Opt3", "Correct!");
+localStorage.setItem("Ques1Opt4", "Wrong!");
+
+var checkAnswer = document.createElement("h1");
+response.appendChild(checkAnswer);
+
+function strtBtn(event) {
     //    document.getElementById("start-description").textContent = "hi";
-    var currentHeader = startTitle.textContent;
+    quesOne(event);
+}
+
+function quesOne(event) {
     startButton.style = "display: none;"
-    if (currentHeader === "Coding 101 Quiz") {
-        startTitle.textContent = "Which syntax is a string written in?";
-        startDescription.textContent = "";
-        option1.textContent = "A) []";
-        option2.textContent = "B) //";
-        option3.textContent = "C) ' '";
-        option4.textContent = "D) ()";
+    startTitle.textContent = "Which syntax is a string written in?";
+    startDescription.textContent = "";
+    q1Option1.textContent = "A) []";
+    q1Option2.textContent = "B) //";
+    q1Option3.textContent = "C) ' '";
+    q1Option4.textContent = "D) ()";
 
-        startDescription.appendChild(option1);
-        startDescription.appendChild(option2);
-        startDescription.appendChild(option3);
-        startDescription.appendChild(option4);
-        startButton = document.createElement('h1');
-        startDescription.appendChild(startButton);
+    console.log(event.target.textContent === q1Option1.textContent);
+    // console.log(q1Option1.textContent);
 
-        option1.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
-        option2.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
-        option3.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
-        option4.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    startDescription.appendChild(q1Option1);
+    startDescription.appendChild(q1Option2);
+    startDescription.appendChild(q1Option3);
+    startDescription.appendChild(q1Option4);
+    startButton = document.createElement('h1');
+    startDescription.appendChild(startButton);
+
+    q1Option1.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    q1Option2.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    q1Option3.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    q1Option4.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");  
+    
+    if (event.target.textContent === q1Option1.textContent) {
+        quesTwo(event);
+    } else if (event.target.textContent === q1Option2.textContent) {
+        quesTwo(event);
+    } else if (event.target.textContent === q1Option3.textContent) {
+        quesTwo(event);
+    } else if (event.target.textContent === q1Option4.textContent) {
+        quesTwo(event);
+    } 
+}
+
+function quesTwo(event) {
+    startTitle.textContent = "How do you define an empty array named myArray?";
+    startDescription.textContent = "";
+    // console.log(response);
+
+    q2Option1.textContent = "A) var myArray = [];";
+    q2Option2.textContent = "B) myArray == []";
+    q2Option3.textContent = "C) Array myArray = ();";
+    q2Option4.textContent = "D) Array myArray = ()";
+    
+    startDescription.appendChild(q2Option1);
+    startDescription.appendChild(q2Option2);
+    startDescription.appendChild(q2Option3);
+    startDescription.appendChild(q2Option4);
+
+    q2Option1.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    q2Option2.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    q2Option3.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    q2Option4.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    
+    // console.log(q1Option1.textContent);
+    // console.log(event.target.textContent === q1Option1.textContent);
+    if (event.target.textContent === q2Option1.textContent) {
+        quesThree(event);
+    } else if (event.target.textContent === q2Option2.textContent) {
+        quesThree(event);
+    } else if (event.target.textContent === q2Option3.textContent) {
+        quesThree(event);
+    } else if (event.target.textContent === q2Option4.textContent) {
+        quesThree(event);
     }
 
-    else if (startTitle.textContent === "Which syntax is a string written in?") {
-        startTitle.textContent = "How do you define an empty array named myArray?";
-        startDescription.textContent = "";
-        option1.textContent = "A) var myArray = [];";
-        option2.textContent = "B) myArray == []";
-        option3.textContent = "C) Array myArray = ();";
-        option4.textContent = "D) Array myArray = ()";
+    if (event.target.textContent === q1Option1.textContent) {
+        checkAnswer.textContent = "Wrong!"
 
-        startDescription.appendChild(option1);
-        startDescription.appendChild(option2);
-        startDescription.appendChild(option3);
-        startDescription.appendChild(option4);
-    }
+        checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
-    else if (startTitle.textContent === "How do you define an empty array named myArray?") {
-        startTitle.textContent = "How do you define an empty function named myFunction?";
-        startDescription.textContent = "";
-        option1.textContent = "A) myFunction";
-        option2.textContent = "B) function myFunction {}";
-        option3.textContent = "function myFunction () {}";
-        option4.textContent = "function = myFunction";
+    } else if (event.target.textContent === q1Option2.textContent) {
+        checkAnswer.textContent = "Wrong!";
 
-        startDescription.appendChild(option1);
-        startDescription.appendChild(option2);
-        startDescription.appendChild(option3);
-        startDescription.appendChild(option4);
-    }
+        checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
-    else if (startTitle.textContent === "How do you define an empty function named myFunction?") {
-        startTitle.textContent = "Which coding language plays the role of the brain of the webpage?";
-        startDescription.textContent = "";
-        option1.textContent = "A) html";
-        option2.textContent = "B) javaScript";
-        option3.textContent = "C) css";
-        option4.textContent = "D) Webpages don't have brains.";
+    } else if (event.target.textContent === q1Option3.textContent) {
+        checkAnswer.textContent = "Correct!";
 
-        startDescription.appendChild(option1);
-        startDescription.appendChild(option2);
-        startDescription.appendChild(option3);
-        startDescription.appendChild(option4);
-    }
+        checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
 
-    else if (startTitle.textContent === "Which coding language plays the role of the brain of the webpage?"){
-        startTitle.textContent = "";
-        startDescription.textContent = "";
+    } else if (event.target.textContent === q1Option4.textContent) {
+        checkAnswer.textContent = "Wrong!";
 
-        startTitle.textContent = "All done!";
-        // document.createElement("div id='score'");
-        console.log(startDescription);
-        // var score = document
-
-    }
-
-    else {
-        // go to other html file
+        checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
     }
 }
-startButton.addEventListener("click", btnClick)
-option1.addEventListener("click", btnClick)
-option2.addEventListener("click", btnClick)
-option3.addEventListener("click", btnClick)
-option4.addEventListener("click", btnClick)
 
+function quesThree(event) {
+    startTitle.textContent = "How do you define an empty function named myFunction?";
+    startDescription.textContent = "";
+    q3Option1.textContent = "A) myFunction";
+    q3Option2.textContent = "B) function myFunction {}";
+    q3Option3.textContent = "C) function myFunction () {}";
+    q3Option4.textContent = "D) function = myFunction";
+
+    startDescription.appendChild(q3Option1);
+    startDescription.appendChild(q3Option2);
+    startDescription.appendChild(q3Option3);
+    startDescription.appendChild(q3Option4);
+
+    q3Option1.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    q3Option2.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    q3Option3.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    q3Option4.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+
+    if (event.target.textContent === q3Option1.textContent) {
+        quesFour(event);
+    } else if (event.target.textContent === q3Option2.textContent) {
+        quesFour(event);
+    } else if (event.target.textContent === q3Option3.textContent) {
+        quesFour(event);
+    } else if (event.target.textContent === q3Option4.textContent) {
+        quesFour(event);
+    }
+
+    if (event.target.textContent === q2Option1.textContent) {
+        checkAnswer.textContent = "Correct!"
+
+        checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
+
+    } else if (event.target.textContent === q2Option2.textContent) {
+        checkAnswer.textContent = "Wrong!";
+
+        checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
+
+    } else if (event.target.textContent === q2Option3.textContent) {
+        checkAnswer.textContent = "Wrong!";
+
+        checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
+
+    } else if (event.target.textContent === q2Option4.textContent) {
+        checkAnswer.textContent = "Wrong!";
+
+        checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
+    }
+}
+
+function quesFour (event) {
+    startTitle.textContent = "Which coding language plays the role of the brain of the webpage?";
+    startDescription.textContent = "";
+    q4Option1.textContent = "A) html";
+    q4Option2.textContent = "B) javaScript";
+    q4Option3.textContent = "C) css";
+    q4Option4.textContent = "D) Webpages don't have brains.";
+
+    startDescription.appendChild(q4Option1);
+    startDescription.appendChild(q4Option2);
+    startDescription.appendChild(q4Option3);
+    startDescription.appendChild(q4Option4);
+
+    q4Option1.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    q4Option2.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    q4Option3.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+    q4Option4.setAttribute("style", "display: flex; background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 30%;");
+
+    if (event.target.textContent === q4Option1.textContent) {
+        scorePage(event);
+    } else if (event.target.textContent === q4Option2.textContent) {
+        scorePage(event);
+    } else if (event.target.textContent === q4Option3.textContent) {
+        scorePage(event);
+    } else if (event.target.textContent === q4Option4.textContent) {
+        scorePage(event);
+    }
+
+    if (event.target.textContent === q3Option1.textContent) {
+        checkAnswer.textContent = "Wrong!"
+
+        checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
+
+    } else if (event.target.textContent === q3Option2.textContent) {
+        checkAnswer.textContent = "Wrong!";
+
+        checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
+
+    } else if (event.target.textContent === q3Option3.textContent) {
+        checkAnswer.textContent = "Correct!";
+
+        checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
+
+    } else if (event.target.textContent === q3Option4.textContent) {
+        checkAnswer.textContent = "Wrong!";
+
+        checkAnswer.setAttribute("style", "display: flex; color: grey; border-top: solid;");
+    }    
+}
+var score = document.createElement("p");
+var initialsContainer = document.createElement("div");
+var initialsLabel = document.createElement("span");
+var initialsPromp = document.createElement("textarea");
+var submitSection = document.createElement("div");
+var submitButton = document.createElement("button");
+
+function scorePage(event) {
+    startTitle.textContent = "All done!";
+    startDescription.textContent = "";
+    checkAnswer.textContent = "";
+
+    startDescription.appendChild(score);
+    startDescription.appendChild(initialsContainer);
+    initialsContainer.appendChild(initialsLabel);
+    initialsContainer.appendChild(initialsPromp);
+    startDescription.appendChild(submitSection);
+    submitSection.appendChild(submitButton);
+
+    submitButton.textContent = "Submit";
+    submitButton.setAttribute("style", "background-color: #100767; color: white; border-radius: 5px; font-size: large; width: 20%;");
+    // console.log(startDescription);
+
+    score.textContent = "Score: ";
+    initialsLabel.textContent = "Initials: "
+    // submitButton.setAttribute("style", "display: flex; align-items: center; width: 5%;");
+}
+
+// function responseOne(event) {
+//     // if (event.target.textContent === "") {
+//     //     startButton.textContent = "Wrong!";
+//     // }
+// }
+startButton.addEventListener("click", strtBtn)
+q1Option1.addEventListener("click", quesOne)
+q1Option2.addEventListener("click", quesOne)
+q1Option3.addEventListener("click", quesOne)
+q1Option4.addEventListener("click", quesOne)
+q2Option1.addEventListener("click", quesTwo)
+q2Option2.addEventListener("click", quesTwo)
+q2Option3.addEventListener("click", quesTwo)
+q2Option4.addEventListener("click", quesTwo)
+q3Option1.addEventListener("click", quesThree)
+q3Option2.addEventListener("click", quesThree)
+q3Option3.addEventListener("click", quesThree)
+q3Option4.addEventListener("click", quesThree)
+q4Option1.addEventListener("click", quesFour)
+q4Option2.addEventListener("click", quesFour)
+q4Option3.addEventListener("click", quesFour)
+q4Option4.addEventListener("click", quesFour)
+// submitButton.addEventListener("click", submitBtn)
 // Remove all elements except for element that contains start button
 
 // Create a timer that will display in the header of the page that subtracts time when
